665e2992e234d199d919789e0534e64978f0e750
heeyomi <yhg9508@gmail.com>
2021-06-25 15:11:18 +0900

    javascript 연습#1

--- javascript-practices/ex02.js
+++ javascript-practices/ex02.js
@@ -1,7 +1,20 @@
 /*
+변수와 데이터 타입
+[기본타입(primitive type), 유사객체]
+1. number
+2. boolean
+3. string
+4. undefined
+[객체]
+*/
+var u; // var u = undefined; 와 동일 자바스크립트 선언과 정의의 구분이 없다. 
+var i = 10;
+var b = true;
+var s = "Hello World";
 
-    변수와 데이터 타입
-    [기본타입(primitive type), 유사객체]
-    
+console.log("u :" + typeof(u));
+console.log("i :" + typeof(i));
+console.log("b :" + typeof(b));
+console.log("s :" + typeof(s));
 
-    */
+console.log(s.toUpperCase());
