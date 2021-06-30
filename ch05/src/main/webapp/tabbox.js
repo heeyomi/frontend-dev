4c24416052325948f7dcb1aeab5547e66f30d72e
heeyomi <yhg9508@gmail.com>
2021-06-30 10:53:27 +0900

    call, apply, bind #1

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -1,26 +1,21 @@
-//DOM Level 1 Event 처리 : elemenet의 event attribute 사용하는 방식
-// 2) Javascript code(DOM API)
 var tabBox = {
-	onTabClicked: function(){
-	},
 	init : function(){
-		liTabs[i].addEventListener("click", onTabClicked);
-	}
-};
-window.addEventListener(load, function() {
-	var divTabBox = document.getElementsByClassName("tab-box")[0];
-	console.log(divTabBox.childNodes);
-		
-	var ul = divTabBox.childNodes[1];
-	var liTabs = ul.getElementsByTagName("li");
-		
-	for (var i = 0; i < liTabs.length; i++) {
-		liTabs[i].addEventListener("click", onTabClicked);
-	}
-});
+		var divTabBox = document.getElementsByClassName("tab-box")[0];
+		var ul = divTabBox.childNodes[1];
+		var liTabs = ul.getElementsByTagName("li");
+				
+		for (var i = 0; i < liTabs.length; i++) {
+			liTabs[i].addEventListener("click", onTabClicked);
+		}
+	}	
+}
 
-console.log("=== global ===");
-window.name = "둘리";
-console.log(name, window.name);
-var email = "dooly@gmail.com";
-console.log(email, window.email);
+
+var onTabClicked = function(){
+	//unselected
+	var lisSelected = document.getElementsByClassName("selected");
+	(lisSelected.length == 1) && (lisSelected[0].className = "");
+	
+	//selected
+	this.className = "selected";
+};
